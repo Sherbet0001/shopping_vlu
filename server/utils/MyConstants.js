ffd6988d810d49f6953c0867217e1cffc81e702d
Sherbet0001 <95078188+Sherbet0001@users.noreply.github.com>
2026-03-30 15:41:36 +0700

    server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.fywh6cu.mongodb.net',
+  DB_USER: 'dat',
+  DB_PASS: '123',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: 'datvienthanh@gmail.com',
+  EMAIL_PASS: 'qkfemwilkemxybyq',
+  JWT_SECRET: 'abciooiqwe',
+  JWT_EXPIRES: '356658435644111',
+};
+
+module.exports = MyConstants;
